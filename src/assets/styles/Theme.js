c1f87ab33fa65f83397ddc6fd98ef9d9fd058d7e
Mateusz <mateuszmatysiak96@gmail.com>
2020-07-08 21:33:50 +0200

    Update Theme and add arrow icon

--- src/assets/styles/Theme.js
+++ src/assets/styles/Theme.js
@@ -1,25 +1,25 @@
-import React from "react"
-import { ThemeProvider } from "styled-components"
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 const theme = {
   palette: {
-    white: "rgba(255, 255, 255, 0.9)",
-    grey: "rgba(148, 148, 149, 0.9)",
+    white: 'rgba(255, 255, 255, 0.9)',
+    grey: 'rgba(148, 148, 149, 0.9)',
     black: {
-      light: "#202022",
-      normal: "#1D1D1F",
-      dark: "#151515",
+      light: '#202022',
+      normal: '#1D1D1F',
+      dark: '#151515',
     },
   },
   breakpoints: {
-    xs: "360px",
-    md: "768px",
-    xl: "1440px",
+    xs: '360px',
+    sm: '768px',
+    md: '1024px',
+    lg: '1440px',
+    xl: '1920px',
   },
-}
+};
 
-const Theme = ({ children }) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
-)
+const Theme = ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 
-export default Theme
+export default Theme;
